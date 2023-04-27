e4c4410c3210c9f7ac1f89e1a873fdb455a0613f
Susmita-Dey <susmitadey475@gmail.com>
2023-04-27 12:28:12 +0530

    Add sidebar

--- internship/components/SideNavbar.jsx
+++ internship/components/SideNavbar.jsx
@@ -1,63 +1,64 @@
+import Link from 'next/link'
 import React from 'react'
+import { AiFillHome, AiTwotoneSetting } from 'react-icons/ai'
+import { BsFire, BsPersonCircle } from 'react-icons/bs'
+import { FaBell, FaBookmark } from 'react-icons/fa'
+import Image from 'next/image'
 
 export default function SideNavbar() {
     return (
-        <div className="min-h-screen flex flex-row bg-gray-100">
-            <div className="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden">
-                <div className="flex items-center justify-center h-20 shadow-md">
-                    <h1 className="text-3xl uppercase text-indigo-500">Logo</h1>
+        <div className="min-h-screen flex flex-row text-primary-text font-medium">
+            <div className="flex flex-col justify-between items-center w-64 bg-white rounded-r-3xl overflow-hidden">
+                <div className="flex items-center justify-center h-20">
+                    <h1 className="text-2xl font-semibold uppercase">Konnektr</h1>
                 </div>
-                <ul className="flex flex-col py-4">
+                <ul className="flex flex-col py-4 justify-center px-10 align-middle">
                     <li>
-                        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-home"></i></span>
-                            <span className="text-sm font-medium">Dashboard</span>
-                        </a>
+                        <Link href="#" className="flex flex-row gap-2 items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                            <AiFillHome className='text-2xl' />
+                            <span className="text-base font-medium">Home</span>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-music"></i></span>
-                            <span className="text-sm font-medium">Music</span>
-                        </a>
+                        <Link href="#" className="flex flex-row gap-2 items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                            <BsFire className="text-2xl" />
+                            <span className="text-base font-medium">Konnektions</span>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-drink"></i></span>
-                            <span className="text-sm font-medium">Drink</span>
-                        </a>
+                        <Link href="#" className="flex flex-row gap-2 items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                            <FaBell className='text-2xl' />
+                            <span className="text-base font-medium">Communities</span>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-shopping-bag"></i></span>
-                            <span className="text-sm font-medium">Shopping</span>
-                        </a>
+                        <Link href="#" className="flex flex-row gap-2 items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                            <FaBookmark className='text-2xl' />
+                            <span className="text-base font-medium">Quests</span>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-chat"></i></span>
-                            <span className="text-sm font-medium">Chat</span>
-                        </a>
+                        <Link href="#" className="flex flex-row gap-2 items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                            <BsPersonCircle className='text-2xl' />
+                            <span className="text-base font-medium">Store</span>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-user"></i></span>
-                            <span className="text-sm font-medium">Profile</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-bell"></i></span>
-                            <span className="text-sm font-medium">Notifications</span>
-                            <span className="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></span>
-                            <span className="text-sm font-medium">Logout</span>
-                        </a>
+                        <Link href="#" className="flex flex-row gap-2 items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 ">
+                            <Image src='/message.svg' width={0} height={0} className="-ml-1 w-1/4 h-1/2" alt='message-icon' />
+                            <span className="text-base font-medium text-gradient">Messages</span>
+                        </Link>
                     </li>
                 </ul>
+                <div className="flex flex-row gap-2 items-center justify-center h-24 bottom-0 rounded-lg shadow-sm">
+                    <Image src={'/user.png'} width={100} height={100} className='w-1/4 h-1/2' alt='user' />
+                    <span className='flex flex-col pr-2'>
+                        <span className="text-base">User</span>
+                        <span className='text-sm text-gradient'>@shivrxj.lens</span>
+                    </span>
+                    <FaBell className='text-xl' />
+                    <AiTwotoneSetting className='text-xl' />
+                </div>
             </div>
         </div>
     )
--- internship/pages/_app.js
+++ internship/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
